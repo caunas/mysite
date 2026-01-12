cbc459f219f17d219cb4028a67ac7d05fc390ba6
Cauan Nascimento <caunas1337@proton.me>
2026-01-12 09:19:49 -0300

    feat: Adição do gerenciamento de posts e projetos via django admin

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -3,3 +3,18 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Post, Project
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'status', 'created_on')
+    list_filter = ('author', 'status')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post)
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name_repo', 'ower', 'status')
+    list_filter = ('owner', 'status')
+    search_fields = ['title', 'description', 'owner']
+    
+admin.site.register(Project)
