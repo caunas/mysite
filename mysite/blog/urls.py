4a9429cbd87d5dcb5b5923ef1d08cccb641e96e9
Cauan Nascimento <caunas1337@proton.me>
2026-01-12 08:31:36 -0300

    Configuração da rota de projetos

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('', views.PostView.as_view(), name = 'home')
+    path('', views.PostView.as_view(), name = 'home'),
+    path('/projects', views.ProjectView.as_view(), name = "projects")
 ]
